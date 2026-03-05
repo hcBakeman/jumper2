24d723c3a0e08729453bda0f5ade544d49e353bc
Riku Silvonen <rsilvone@gmail.com>
2026-03-05 19:36:44 +0200

    Add fb config

---
+++ config/firebase-config.js
@@ -0,0 +1,17 @@
+// Firebase Configuration
+// This file contains the Firebase project configuration.
+// NOTE: Firebase API keys are safe to expose in client-side code.
+// Security is enforced through Firebase Security Rules, not API key secrecy.
+// See: https://firebase.google.com/docs/projects/api-keys
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBSYkTk7kmG0RnNfDrTGWoKeU48I8UMjPk",
+    authDomain: "jumper2-8d29a.firebaseapp.com",
+    databaseURL: "https://jumper2-8d29a-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "jumper2-8d29a",
+    storageBucket: "jumper2-8d29a.firebasestorage.app",
+    messagingSenderId: "216269698862",
+    appId: "1:216269698862:web:dacb4ae12ee16cf57cacbf",
+    measurementId: "G-WDYBX7DQHF"
+};
+
